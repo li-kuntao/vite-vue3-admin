39e8bbab0af3dafa3356b6109b70d0553c761eb2
li-kuntao <15811515011@163.com>
2022-08-19 16:05:05 +0800

    docs: other 修改README内容

--- .cz-config.js
+++ .cz-config.js
@@ -1,17 +1,17 @@
 module.exports = {
   // type 类型（定义之后，可通过上下键选择）
   types: [
-    { value: 'feat', name: 'feat:     新增功能' },
-    { value: 'fix', name: 'fix:      修复 bug' },
-    { value: 'docs', name: 'docs:     文档变更' },
-    { value: 'style', name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）' },
+    { value: 'feat', name: 'feat: 新增功能' },
+    { value: 'fix', name: 'fix: 修复 bug' },
+    { value: 'docs', name: 'docs: 文档变更' },
+    { value: 'style', name: 'style: 代码格式（不影响功能，例如空格、分号等格式修正）' },
     { value: 'refactor', name: 'refactor: 代码重构（不包括 bug 修复、功能新增）' },
-    { value: 'perf', name: 'perf:     性能优化' },
-    { value: 'test', name: 'test:     添加、修改测试用例' },
-    { value: 'build', name: 'build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）' },
-    { value: 'ci', name: 'ci:       修改 CI 配置、脚本' },
-    { value: 'chore', name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
-    { value: 'revert', name: 'revert:   回滚 commit' }
+    { value: 'perf', name: 'perf: 性能优化' },
+    { value: 'test', name: 'test: 添加、修改测试用例' },
+    { value: 'build', name: 'build: 构建流程、外部依赖变更（如升级 npm 包、修改 vite 配置等）' },
+    { value: 'ci', name: 'ci: 修改 CI 配置、脚本' },
+    { value: 'chore', name: 'chore: 对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
+    { value: 'revert', name: 'revert: 回滚 commit' }
   ],
 
   // scope 类型（定义之后，可通过上下键选择）
@@ -60,7 +60,7 @@ module.exports = {
 
   // 交互提示信息
   messages: {
-    type: '确保本次提交遵循 Angular 规范！\n选择你要提交的类型：',
+    type: '确保本次提交遵循 Vue 规范！\n选择你要提交的类型：',
     scope: '\n选择一个 scope（可选）：',
     // 选择 scope: custom 时会出下面的提示
     customScope: '请输入自定义的 scope：',
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# vite-vue3-admin
+# Preparation
+
+* 编程语言：TypeScript 4.x + JavaScript
+* 构建工具：Vite 2.x
+* 前端框架：Vue 3.x
+* 路由工具：Vue Router 4.x
+* 状态管理：Pinia 2.x
+* UI 框架：Element Plus
+* CSS 预编译：Sass
+* HTTP 工具：Axios
+* Git Hook 工具：husky + lint-staged
+* 代码规范：EditorConfig + Prettier + ESLint + Airbnb JavaScript Style Guide
+* 提交规范：Commitizen + Commitlint
+* 自动部署：GitHub Actions
+
+# Getting started
+
+## clone the project
+git clone https://github.com/li-kuntao/vite-vue3-admin.git
+
+## enter the project directory
+cd vite-vue3-admin
+
+## install dependency
+npm install
+
+## develop
+npm run dev
+
+# Build
+
+npm run build
+
+# Advanced
+
+## preview
+npm run preview
+
+## prepare
+npm run prepare
+
+## lint
+npm run lint
+
